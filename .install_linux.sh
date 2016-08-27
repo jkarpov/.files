530801fedb8d3d3b8efa5f29bcb675c870d20348
Dmitriy Tadyshev <dmitriy@tadyshev.com>
2016-08-27 07:18:03 -0500

    Remove pre-existing dot files

--- .install_linux.sh
+++ .install_linux.sh
@@ -5,13 +5,13 @@ git clone --bare https://github.com/ditadi/.linux.git $HOME/.linux
 function config {
   /usr/bin/git --git-dir=$HOME/.linux --work-tree=$HOME $@
 }
-mkdir -p .linux-backup
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out .linux.";
 else
-  echo "Backing up pre-existing dot files.";
-  config checkout 2>&1 | egrep "\s+\." | awk {'print$1'} | xargs -I{} mv {} .linux-backup/{}
+  echo "Removing pre-existing dot files.";
+  config checkout 2>&1 | egrep "\s+\." | awk {'print$1'} \
+  xargs -I{} rm {} 
 fi;
 config checkout
 config config status.showUntrackedFiles no
