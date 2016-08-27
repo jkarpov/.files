307b99981881aefe0eb4947dca2210bf8003244d
Dmitriy Tadyshev <dmitriy@tadyshev.com>
2016-08-27 06:21:50 -0500

    Add config alias to bashrc

--- .linux_install.sh
+++ .linux_install.sh
@@ -1,3 +1,6 @@
+alias config='/usr/bin/git --git-dir $HOME/.linux/ --work-tree=$HOME' >> $HOME/.bashrc
+. ~/.bashrc
+echo ".linux" >> .gitignore
 git clone --bare https://github.com/ditadi/.linux.git $HOME/.linux
 function config {
   /usr/bin/git --git-dir=$HOME/.linux --work-tree=$HOME $@
