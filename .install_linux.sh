9e902cef380fa352d71baaf30d6939288df43484
Dmitriy Tadyshev <dmitriy@tadyshev.com>
2016-08-27 07:09:47 -0500

    Rename install script, fix xargs switch

--- .install_linux.sh
+++ .install_linux.sh
@@ -11,7 +11,7 @@ if [ $? = 0 ]; then
   echo "Checked out .linux.";
 else
   echo "Backing up pre-existing dot files.";
-  config checkout 2>&1 | egrep "\s+\." | awk {'print$1'} | xargs -l{} mv {} .linux-backup/{}
+  config checkout 2>&1 | egrep "\s+\." | awk {'print$1'} | xargs -I{} mv {} .linux-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
