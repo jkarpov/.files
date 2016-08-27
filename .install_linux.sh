dcadbbad601a27261e806727140954dfaf01b87e
Dmitriy Tadyshev <dmitriy@tadyshev.com>
2016-08-27 07:22:06 -0500

    Fix xargs multi line

--- .install_linux.sh
+++ .install_linux.sh
@@ -10,8 +10,7 @@ if [ $? = 0 ]; then
   echo "Checked out .linux.";
 else
   echo "Removing pre-existing dot files.";
-  config checkout 2>&1 | egrep "\s+\." | awk {'print$1'} \
-  xargs -I{} rm {} 
+  config checkout 2>&1 | egrep "\s+\." | awk {'print$1'} | xargs -I{} rm {} 
 fi;
 config checkout
 config config status.showUntrackedFiles no
