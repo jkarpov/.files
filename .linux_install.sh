61ed6317317b27c023374c0034d835cc6c883fdb
Dmitriy Tadyshev <dmitriy@tadyshev.com>
2016-08-27 06:52:53 -0500

    Change mv to cp and rm for recursive directories

--- .linux_install.sh
+++ .linux_install.sh
@@ -11,7 +11,7 @@ if [ $? = 0 ]; then
   echo "Checked out .linux.";
 else
   echo "Backing up pre-existing dot files.";
-  config checkout 2>&1 | egrep "\s+\." | awk {'print$1'} | xargs -I{} mv {} .linux-backup/{}
+  config checkout 2>&1 | egrep "\s+\." | awk {'print$1'} | xargs -I{} cp -R {} .linux-backup/{} rm -r {}
 fi;
 config checkout
 config config status.showUntrackedFiles no
