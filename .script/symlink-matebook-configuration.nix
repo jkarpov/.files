ae60e99f7a3da88b5e8c8f0441147eb80143487e
Dmitriy Tadyshev <dmitriy@tadyshev.com>
2019-01-12 14:59:36 -0600

    bump

---
+++ .script/symlink-matebook-configuration.nix
@@ -0,0 +1,1 @@
+ln -s /home/ditadi/.config/nixos/matebook-configuration.nix /etc/nixos/configuration.nix
---
+++ .script/symlink-pc-configuration.nix
@@ -0,0 +1,1 @@
+ln -s /home/ditadi/.config/nixos/pc-configuration.nix /etc/nixos/configuration.nix
