d3a14b3e3edeee1c0ccfefd6e2e91f5d450dd29c
Dmitriy Tadyshev <dmitriy@tadyshev.com>
2019-01-24 20:24:40 -0600

    make bash the default shell

--- .config/nixos/all-users.nix
+++ .config/nixos/all-users.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 {
+  users.defaultUserShell = pkgs.bash;
   users.extraUsers.ditadi = {
     uid = 1000;
     isNormalUser = true;
@@ -12,6 +13,7 @@
     ];
     createHome = true;
     home = "/home/ditadi";
+    useDefaultShell = true;
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfzW8cMR/+eRv7tCWc7GZ/DmmzjhowgogBgm//Xi4r7yfwJyzOkWyMR/CLFQR/83qRRZV3VteSF3R8HYNjL4Rl6lYCN5pRUbcAgU9ZXxeEAVGXDVHUr2rxNBDfIzimFDCiwRQzM2XOoCEEd2oi7FyXGJ+VV16LqKkSmDtPfN9BP+bUYVRqzDY57Y6Mjlv0G66T5GegyJ/lEI5sL/Y5wVtIOcHkSMkpq4QhW6X8CIYsQrve2Derx/d2pqWDhuMhxi+D8P2x0KjztwIHuNjxRP8sHg/e4jC1+ambu/OwqRDQfdYM8yK5AUt9PRR5pd9myPHtKlkuXZsKdPMhgmlvemA5 dmitriy@tadyshev.com"
     ];
