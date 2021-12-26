3658c55ace8562a44a534950db9fe22e5872d013
Dmitriy Tadyshev <dmitriy@tadyshev.com>
2021-12-26 12:37:40 -0800

    Fix sea flake.nix syntax error

--- flake.nix
+++ flake.nix
@@ -7,33 +7,31 @@
   outputs = { home-manager, nixpkgs, ... }: {
 
     nixosConfigurations.sea = nixpkgs.lib.nixosSystem {
-      sea = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ 
-          ./hardware-configuration.sea.nix
-          ./configuration.sea.nix 
-          ./users.nix 
-          home-manager.nixosModules.home-manager 
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ditadi = import ./home.ditadi.nix;
-          }
-        ];
-      };
+      system = "x86_64-linux";
+      modules = [ 
+        ./hardware-configuration.sea.nix
+        ./configuration.sea.nix 
+        ./users.nix 
+        home-manager.nixosModules.home-manager 
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.ditadi = import ./home.ditadi.nix;
+        }
+      ];
     };
 
     nixosConfigurations.ec2 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ 
-	        ./configuration.ec2.nix 
-          ./users.nix
-	        home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ditadi = import ./home.ditadi.nix;
-          }
+      system = "x86_64-linux";
+      modules = [ 
+        ./configuration.ec2.nix 
+        ./users.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.ditadi = import ./home.ditadi.nix;
+        }
 	    ];
     };
 
