13756823316b5ba085e4e6758eb362049f4b1166
Dmitriy Tadyshev <dmitriy@tadyshev.com>
2021-12-26 11:58:39 -0800

    Rename: home.nix -> home.ditadi.nix

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.ditadi = import ./home.nix;
+          home-manager.users.ditadi = import ./home.ditadi.nix;
         }
       ];
     };
