dcde3a7e9278519e22ff723877a68f0d392033cb
Dmitriy Tadyshev <dmitriy@tadyshev.com>
2021-12-26 20:33:40 +0000

    Configuration for ec2 instance

---
+++ configuration.ec2.nix
@@ -0,0 +1,23 @@
+{ modulesPath, pkgs, ... }: 
+
+{
+  imports = [ 
+	"${modulesPath}/virtualisation/amazon-image.nix" 
+  ];
+
+  ec2.hvm = true;
+  systemd.services.amazon-init.enable = false;
+  networking.hostName = "ec2";  
+
+  services.tailscale.enable = true;
+  networking.firewall.allowedUDPPorts = [ 41641 ];
+  environment.systemPackages = [ pkgs.tailscale ];
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+   };
+
+}
